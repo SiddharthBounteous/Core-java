bb125056383626fded7dc28190035a019e5c9ab1
Siddharth Shukla <siddharth.shukla@bounteous.com>
2026-03-08 00:01:22 +0530

    ForkJoin pool executor done

---
+++ OOPs/src/Multihreading/Executor/ExecutorUtilityExample.java
@@ -0,0 +1,56 @@
+package Multihreading.Executor;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+
+class ComputeSumTask extends RecursiveTask<Integer>{
+    int start;
+    int end;
+
+    ComputeSumTask(int start,int end){
+        this.start=start;
+        this.end=end;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (end - start <= 4) {
+            int totalSum = 0;
+            for(int i = start; i <= end; i++){
+                totalSum += i;
+            }
+            return totalSum;
+        }
+        else {
+            // Split the task
+            int mid = (start + end) / 2;
+            ComputeSumTask leftTask = new ComputeSumTask(start, mid);
+            ComputeSumTask rightTask = new ComputeSumTask(mid + 1, end);
+
+            // Fork the subtasks for parallel execution
+            leftTask.fork();
+            rightTask.fork();
+
+            // Combine the results of subtasks
+            int leftResult = leftTask.join();
+            int rightResult = rightTask.join();
+
+            // Combine the results
+            return leftResult + rightResult;
+        }
+    }
+}
+public class ExecutorUtilityExample {
+
+    static void main(String[] args) {
+        ForkJoinPool pool=ForkJoinPool.commonPool();
+        Future<Integer> futureObj=pool.submit(new ComputeSumTask(0,100));
+        try{
+            System.out.println(futureObj.get());
+        }
+        catch (Exception ex){
+
+        }
+    }
+}
